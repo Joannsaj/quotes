c4a7e353e44a98055c8de11d695969c6d80d78fd
joansaj <joannsaj@gmail.com>
2020-09-26 00:30:49 +0300

    add new quote logic

--- IPcore/quote/src/app/components/quote-form/quote-form.component.ts
+++ IPcore/quote/src/app/components/quote-form/quote-form.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { IQuote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
-export class QuoteFormComponent implements OnInit {
+export class GoalFormComponent implements OnInit {
+  newQuote = new IQuote(0, "", "", new Date());
+  @Output() addGoal = new EventEmitter<IQuote>();
+
+
 
   constructor() { }
 
