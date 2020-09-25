2af75291660c5ffc01fafbc13fedb64c8e91b7e2
joansaj <joannsaj@gmail.com>
2020-09-25 17:48:23 +0300

    created components

---
+++ IPcore/quote/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    QuoteFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
